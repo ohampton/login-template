9abffde792c6ff0fd083f68d80c00f6a5e94a728
ohampton <44102431+ohampton@users.noreply.github.com>
2021-02-16 18:39:46 -0500

    commit

---
+++ main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMcontentLoaded", () => {
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+
+    document.querySelector("#linkCreateAccount").addEventListener("click" , e => {
+        e.preventDefault();
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden"); 
+    });
+    
+    document.querySelector("#linklogin").addEventListener("click" , e => {
+        e.preventDefault();
+        loginForm.classList.remove("form--hidden");
+        createAccountForm.classList.add("form--hidden"); 
+    });
+});
