3bf5049c669b5bd5b45586e2e55c78893215d790
ohampton <44102431+ohampton@users.noreply.github.com>
2021-02-16 19:22:03 -0500

    delete

--- main.js
+++ main.js
@@ -55,30 +55,3 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-// document.addEventListener("DOMcontentLoaded", () => {
-//     const loginForm = document.querySelector("#login");
-//     const createAccountForm = document.querySelector("#createAccount");
-
-//     document.querySelector("#linkCreateAccount").addEventListener("click" , e => {
-//         e.preventDefault();
-//         loginForm.classList.add("form--hidden");
-//         createAccountForm.classList.remove("form--hidden"); 
-//     });
-    
-//     document.querySelector("#linklogin").addEventListener("click" , e => {
-//         e.preventDefault();
-//         loginForm.classList.add("form--hidden");
-//         createAccountForm.classList.remove("form--hidden"); 
-//     });
-// });
