542f5c0c51bed9008691aae1114ed3260fe9fa8e
ohampton <44102431+ohampton@users.noreply.github.com>
2021-02-19 12:11:14 -0500

    functionCommit

--- server.js
+++ server.js
@@ -98,6 +98,36 @@ MongoClient.connect(connectionString,{useUnifiedTopology: true}) .then(client =>
       });
     });
   }
+
+  function login(email, password, callback) {
+    
+  
+    client.connect(function (err) {
+      if (err) return callback(err);
+  
+      const db = client.db('db-name');
+      const users = db.collection('users');
+  
+      users.findOne({ email: email }, function (err, user) {
+        if (err || !user) {
+          client.close();
+          return callback(err || new WrongUsernameOrPasswordError(email));
+        }
+  
+        bcrypt.compare(password, user.password, function (err, isValid) {
+          client.close();
+  
+          if (err || !isValid) return callback(err || new WrongUsernameOrPasswordError(email));
+  
+          return callback(null, {
+              user_id: user._id.toString(),
+              nickname: user.nickname,
+              email: user.email
+            });
+        });
+      });
+    });
+  }
   
   //Create User Function
   function create(user, callback) {
